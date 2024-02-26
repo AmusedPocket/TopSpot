815a36208d60b778e1498577d5803a8a22602845
Trevor <trevirv@gmail.com>
2024-02-25 22:03:35 -0800

    wiki docs

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
@@ -10,9 +11,23 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    firstName = db.Column(db.string(255), nullable=False)
+    lastName = db.Column(db.string(255), nullable=False)
+    userName = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    profilePic = db.Column(db.string(255))
+    createdAt = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    updatedAt = db.Column(db.DateTime, nullable=False, default=datetime.now())
+
+    followers = db.relationship(
+        "User",
+        secondary="follows",
+        primaryjoin=(follows.c.follower_id == id),
+        secondaryjoin=(follows.c.user_id == id),
+        backref=db.backref("follows", lazy="dynamic"),
+        lazy="dynamic"
+    )
 
     @property
     def password(self):
@@ -28,6 +43,8 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
+            'firstName': self.firstName,
+            'lastName': self.lastName,
             'username': self.username,
             'email': self.email
         }
