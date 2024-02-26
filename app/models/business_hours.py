f3de57d87fe75d322fb137a27b3f4b06ea3069ac
Trevor <trevirv@gmail.com>
2024-02-26 15:26:05 -0800

    fixing seeds

--- app/models/business_hours.py
+++ app/models/business_hours.py
@@ -2,7 +2,7 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime
 
 class BusinessHour(db.Model):
-    __tablename__ = 'business hours'
+    __tablename__ = 'business_hours'
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
